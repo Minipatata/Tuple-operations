5b9df10659f1b8d0670875223a0ec020f88d644a
Minipatata <ashleynaemmalee3827@ele.cssda.ca>
2025-12-07 12:27:09 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#Write a program to perform the following operations: 
+# 1. Create a tuple with different datatypes 
+# 2. Create another tuple of integers 
+# 3. Create a new tuple by adding 9 to the previous tuple 
+# 4. Count the occurrences of an element in the tuple 
+# 5. Perform slicing on the tuple
+a=(1,2,True,5.3,2,"Ashleyna",2,"Pie",15.3,2)
+b=(15,16,17,18,19,21)
+#We cannot perform this operator
+print(a.count(2))
+print(a[2:7])
